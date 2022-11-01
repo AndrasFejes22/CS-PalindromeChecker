09d5be1a12656fe150a18ccc092c6fedcfe52db9
AFejes22 <afejes22@gmail.com>
2022-11-01 16:24:57 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IsPalindrome("indulagörögaludni");
+            Console.WriteLine();
+            IsPalindrome("koromfekete");
+            Console.ReadLine();
+        }
+
+        private static void IsPalindrome(string str) 
+        { 
+            Console.WriteLine("The word is: " +str);
+
+            Console.WriteLine();
+
+            char[] chars = str.ToCharArray();
+
+            Array.Reverse(chars);
+
+            string reversedString = new string(chars);
+
+            if (str.Equals(reversedString))
+            {
+                Console.WriteLine(str + " is palindrome.");
+            }
+            else 
+            {
+                Console.WriteLine(str + " is not palindrome.");
+            }
+
+        }
+    }
+}
